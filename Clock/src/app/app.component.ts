e064da79128dac6fc555a0d682fea89afb9137d1
gruiton <julen.ormaetxea81@gmail.com>
2017-04-13 13:08:56 +0200

    clock implementation

--- Clock/src/app/app.component.ts
+++ Clock/src/app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	selector : 'my-app',
+	templateUrl : 'app/html/app.component.html',
+	styleUrls : ['app/css/app.component.css']
+
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	timer;
+	hours : string;
+	minutes : string;
+	seconds : string;
+	hoursRotation : number;
+	minutesRotation : number;
+	secondsRotation : number;
+	constructor() {
+		this.getTime();
+		setInterval(() => { this.getTime(); }, 1000);
+	}
+	getTime() : void{
+		var now = new Date();
+		this.hours = now.getHours();
+		this.minutes = now.getMinutes();  
+		this.seconds = now.getSeconds();
+		this.setRotation();		
+	}
+	setRotation() : void {
+		this.hoursRotation = this.hours * 6;
+		this.minutesRotation = this.minutes * 6;
+		this.secondsRotation = this.seconds * 6;
+	}
+	digitalFormat(input : number) : string{
+		return ("0" + input).slice(-2);
+	}
+}
